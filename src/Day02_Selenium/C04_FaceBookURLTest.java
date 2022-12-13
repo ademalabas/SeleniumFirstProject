f51534ccab47a99783a31ca00bc29feca4180639
ademalabas@gmail.com <ademalabas@gmail.com>
2022-12-13 23:46:59 +0300

    13.12.22

--- src/Day02_Selenium/C04_FaceBookURLTest.java
+++ src/Day02_Selenium/C04_FaceBookURLTest.java
@@ -3,7 +3,7 @@ package Day02_Selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class C03_FaceBookURLTest {
+public class C04_FaceBookURLTest {
 
     public static void main(String[] args) {
         // Invoke Chrome Driver
---
+++ src/Day02_Selenium/C05_PageSourceTest.java
@@ -0,0 +1,36 @@
+package Day02_Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C05_PageSourceTest {
+    public static void main(String[] args) {
+// Go to the Amazon URL: https://www.amazon.com/
+        // Verify that it writes "Performance Metrics" and "MEOW" from the Source code.
+
+        //  Driver imizin yolunu javaya tanittik.
+        System.setProperty("webdriver.chrome.driver","Dependencies\\drivers\\chromedriver.exe");
+
+        //Driver objemizi olusturuyoruz
+        WebDriver driver = new ChromeDriver();
+
+        //amazon sayfamiza gidiyoruz
+        driver.get("https://www.amazon.com/");
+        // page sorcu aldik
+        String pageSource = driver.getPageSource();
+        // page source testimizi yaptik
+        System.out.println("Page test source testi yapiliyor");
+
+        if (pageSource.contains("Performance Matrics") && pageSource.contains("MEOW")){
+            System.out.println("test passed");
+        }else System.out.println("test failed");
+
+        // driverimizi kapattik
+        driver.quit();
+
+
+
+    }
+
+
+}
